46392d77afb12c449b53418fab3e147c30f071b2
Linoxyr <kestascachard@gmail.com>
2025-06-24 20:29:27 +0200

    Amélioration de la fonction create_connect_embed pour inclure la récupération dynamique des emojis personnalisés en fonction du rang du joueur, tout en assurant la cohérence des données avec l'utilisation de "Discord_PlayerID".

---
+++ HelloWorldPluginCS2/Class1.cs
@@ -0,0 +1,18 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+
+namespace HelloWorldPluginCS2;
+
+public class MonPlugin : BasePlugin
+{
+    public override string ModuleName => "yoloooooo";
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "Linox";
+    public override string ModuleDescription => "Un plugin test pour CS2";
+
+    public override void Load(bool hotReload)
+    {
+        Console.WriteLine("Plugin CS2 chargé !");
+        Server.PrintToConsole("MonPluginCS2 est actif !");
+    }
+}
